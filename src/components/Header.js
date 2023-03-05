88ecb5719bc699ec589a3263acc4d8c724e6c22e
curtyraissa <raissacurty@gmail.com>
2023-03-05 00:51:45 -0300

    criado componente header com estilizacao

---
+++ src/components/Header.js
@@ -0,0 +1,36 @@
+import styled from "styled-components"
+
+export const Header = () => {
+    return (
+      <ContainerHeader>
+          <img src="./assets/logo.png" alt="logo zap recall"></img>
+          <p>ZapRecall</p>
+      </ContainerHeader>
+    )
+  }
+
+const ContainerHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+  gap: 30px;
+  height: 100px;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  z-index: 1;
+  background-color: #FB6B6B;
+
+  img {
+    width: 52px;
+    height: 60px;
+  }
+
+  p {
+    font-family: 'Righteous', cursive;
+    font-weight: 400;
+    font-size: 36px;
+    color: #ffffff;
+  }
+`
