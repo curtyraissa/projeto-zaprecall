694483a69c888817f73a260ceb1e2beb506db3b2
curtyraissa <raissacurty@gmail.com>
2023-03-05 00:52:07 -0300

    fix estilizacao componente contador

--- src/components/Contador.js
+++ src/components/Contador.js
@@ -3,7 +3,7 @@ import styled from "styled-components"
 export const Contador = () => {
   return (
     <ContainerContador>
-      <p>0/4 concluído</p>
+      <p>0/8 concluído</p>
     </ContainerContador>
   )
 }
@@ -12,10 +12,13 @@ const ContainerContador = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-
   background-color: #FFFFFF;
   height: 70px;
   box-shadow: 0px -4px 6px rgba(0, 0, 0, 0.05);
+  position: fixed;
+  bottom: 0;
+  z-index: 1;
+  width: 100%;
 
   p {
     font-family: 'Recursive', sans-serif; 
