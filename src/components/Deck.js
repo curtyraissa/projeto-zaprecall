98dc3a10197c18f61b066e24a7a850bba26644b4
curtyraissa <raissacurty@gmail.com>
2023-03-05 00:52:42 -0300

    criado estilizacao do componente deck

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -1,5 +1,30 @@
+import styled from "styled-components"
+import { Flashcards } from "./Flashcards"
+import { Header } from "./Header"
+
 export const Deck = () => {
     return (
-      <>Deck</>
+      <ContainerDeck>
+        <Flashcards/>
+        <Flashcards/>
+        <Flashcards/>
+        <Flashcards/>
+        <Flashcards/>
+        <Flashcards/>
+        <Flashcards/>
+        <Flashcards/>
+      </ContainerDeck>
     )
   }
+
+const ContainerDeck = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 25px;
+  padding: 5 35px;
+  overflow-y: scroll;
+  margin-top: 100px;
+  margin-bottom: 75px;
+`
