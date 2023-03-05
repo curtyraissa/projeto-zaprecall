2a87f9b2cd6c62941659b722b4fd9f08106361ec
curtyraissa <raissacurty@gmail.com>
2023-03-05 00:52:27 -0300

    criado estilizacao do componente flashcards

--- src/components/Flashcards.js
+++ src/components/Flashcards.js
@@ -1,5 +1,33 @@
+import styled from "styled-components"
+
 export const Flashcards = () => {
   return (
-    <>Flashcards</>
+    <ContainerFlashcard>
+      <p>Pergunta 1</p>
+      <img src="./assets/seta_play.png" alt="seta play"></img>
+    </ContainerFlashcard>
   )
 }
+
+const ContainerFlashcard = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  background-color: #ffffff;
+  width: 300px;
+  height: 65px;
+  box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
+  border-radius: 5px;
+
+  p {
+    font-family: 'Recursive', sans-serif; 
+    font-weight: 700;
+    font-size: 16px;
+    color: #333333;
+  }
+
+  img {
+    width: 20px;
+    height: 23px;
+  }
+`
